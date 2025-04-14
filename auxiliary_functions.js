81615b27c6754b117eeaa74bb3631c96f8e8d63d
Jonas Fortes <jonascforte@hotmail.com>
2025-04-14 17:09:54 -0300

    feat: readFile, writeFile functions

--- auxiliary_functions.js
+++ auxiliary_functions.js
@@ -1,7 +1,45 @@
+import fs from "fs/promises"
+const caminhoDoArquivo = "./tarefas.json"
 
 export const mostrarMenu = () => {
     console.log(`       
         Menu de Operações:
         
         `)
+}
+
+
+export async function lerTarefas() {
+
+    try {
+         //ler as tarefas dentro do arquivo tarefas.json
+        const dadosLidos = await fs.readFile(caminhoDoArquivo, "utf-8")
+        return JSON.parse(dadosLidos)
+    } catch (error) {
+        console.error(`Erro ao ler o arquivo! ${error.message}`)
+        return undefined
+    }
+   
+}
+
+
+export async function escreverNoArquivo(listaTarefas){
+
+    const listaTarefasString = JSON.stringify(listaTarefas, null, 2)
+
+    try {
+        await fs.writeFile(caminhoDoArquivo, listaTarefasString, "utf-8")
+        console.log("Arquivo Atualizado!")
+    } catch (error) {
+        console.error(`Erro ao escrever no arquivo! ${error.message}`)
+    }
+
+}
+
+export async function mostrarTodasTarefas () {
+
+    const todasAsTarefas = await lerTarefas()
+
+    console.log(todasAsTarefas)
+
 }
--- main.js
+++ main.js
@@ -1,5 +1,23 @@
-import {mostrarMenu} from './auxiliary_functions.js'
+import {mostrarMenu, lerTarefas, escreverNoArquivo, mostrarTodasTarefas} from './auxiliary_functions.js'
 
-mostrarMenu()
+
+
+// console.log(typeof await lerTarefas())
+
+// console.log(await lerTarefas())
+
+
+// const tarefa01 = {
+//     id: "1",
+//     titulo: "FAzer Almoço",
+//     descricao: "Receita macarronada",
+//     concluida: false
+// }
+
+// await escreverNoArquivo([tarefa01])
+
+await mostrarTodasTarefas()
+
+
 
 
